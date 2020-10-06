bee968c96914c393828c827fa05c65cacca4b72a
KyaBacchaHai <55993921+KyaBacchaHai@users.noreply.github.com>
2020-10-06 10:12:08 +0530

    Defined all Retrival methods

---
+++ phase_4/Scripts/Retrival/aggregate.py
@@ -0,0 +1,10 @@
+def average_number_of_subscribers_for_a_volume ():
+    pass
+
+
+def total_number_of_articles_in_a_volume ():
+    pass
+
+
+def subscription_change_between_two_given_dates ():
+    pass
---
+++ phase_4/Scripts/Retrival/analysis.py
@@ -0,0 +1,6 @@
+def most_frequent_category_of_articles_written_by_each_author ():
+    pass
+
+
+def list_of_subscribers_who_subscribed_between_two_consecutive_volumes ():
+    pass
