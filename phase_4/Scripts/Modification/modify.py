5192eaf5cbc98582fc995d2739898a6d4e35419f
KyaBacchaHai <55993921+KyaBacchaHai@users.noreply.github.com>
2020-10-06 12:47:20 +0530

    Added Modification methods

--- phase_4/Scripts/Modification/modify.py
+++ phase_4/Scripts/Modification/modify.py
@@ -4,7 +4,7 @@ import Scripts.Modification.insertion as insertion
 import Scripts.Modification.updation as updation
 
 def main():
-	print("Which of the following Modification would you like to do?")
+	print("Which of the following Modifications would you like to do?")
 	print("""1. Insertion
 		Creating an Issue.
 		Adding Tags to Article.
@@ -29,3 +29,55 @@ def main():
 	ch = int(input("Enter choice: "))
 	tmp = sp.call('clear', shell = True)
 
+	if ch==1:
+		print("Which of the following Insertions would you like to do?")
+		print("""
+		1. Creating an Issue.
+		2. Adding Tags to Article.
+		3. Adding Categories to the Article.
+		4. Adding Tags to the Article.
+		5. Add a Member.
+		6. Add a Subscriber.
+		7. Add a Sponsor.""")
+		
+		ch2 = int(input("Enter choice: "))
+		tmp = sp.call('clear', shell = True)
+
+		# 
+		# 
+		# 
+
+	elif ch==2:
+		print("Which of the following Updations would you like to do?")
+		print("""
+		1. Modify the contents of the Article.
+		2. Change the issue of an Article.
+		3. Change the tags in an Article.
+		4. Change the categories in an Article.
+		5. Modify Member details.
+		6. Modify Subscriber details.""")
+
+		ch2 = int(input("Enter choice: "))
+		tmp = sp.call('clear', shell = True)
+
+		# 
+		# 
+		# 
+
+	elif ch==3:
+		print("Which of the following Deletions would you like to do?")
+		print("""
+		1. Remove a Member.
+		2. Delete an Article.
+		3. Unsubscribe.
+		4. Remove a Sponsor""")
+
+		ch2 = int(input("Enter choice: "))
+		tmp = sp.call('clear', shell = True)
+
+		# 
+		# 
+		# 
+
+	else:
+		return 
--- phase_4/Scripts/Retrival/retrive.py
+++ phase_4/Scripts/Retrival/retrive.py
@@ -41,4 +41,82 @@ def main():
 
 	ch = int(input("Enter choice: "))
 	tmp = sp.call('clear', shell = True)
+
+	if ch==1:
+		print("Which of the following Selections would you like to perform?")
+		print("""
+		1. List all Issues.
+		2. List all Articles.
+		3. List all Authors.
+		4. Articles from a particular Magazine Issue.
+		5. List of all Articles written by an Author.
+		6. List all Editors.
+		7. List of all Articles edited by an Editor.
+		8. List all Designers.
+		9. List of all Articles designed by a Designer.
+		10.Show Cartoons by Designer.
+		11.Show Cartoons from a particular Issue.
+		12.Show Cartoons from a particular Volume.
+		13.Subscribers for a given Volume.
+		14.Subscribers for a given Issue.""")
+		ch2 = int(input("Enter choice: "))
+		tmp = sp.call('clear', shell = True)
 	
+		# 
+		# 
+		# 
+
+	elif ch==2:
+		print("Which of the following Projections would you like to perform?")
+		print("""
+		1. Issues in a Volume.
+		2. Articles with a particular category.
+		3. Sort Articles by name.
+		4. Show Cartoons with a particular Tag.
+		5. Subscribers with a particular subscription type""")
+		ch2 = int(input("Enter choice: "))
+		tmp = sp.call('clear', shell = True)
+
+		# 
+		# 
+		# 
+
+	elif ch==3:
+		print("Which of the following Aggregates would you like to perform?")
+		print("""
+		1. Average number of subscribers for a Volume.
+		2. Total number of articles in a Volume.
+		3. Subscription Change between two given dates.""")
+		ch2 = int(input("Enter choice: "))
+		tmp = sp.call('clear', shell = True)
+
+		# 
+		# 
+		# 
+
+	elif ch==4:
+		print("Which of the following Searches would you like to perform?")
+		print("""
+		1. Search Articles by name.
+		2. Search Members by name""")
+		ch2 = int(input("Enter choice: "))
+		tmp = sp.call('clear', shell = True)
+
+		# 
+		# 
+		# 
+
+	elif ch==5:
+		print("Which of the following Analysis would you like to perform?")
+		print("""
+		1. Most frequent category of Articles written by each Author.
+		2. List of subscribers who subscribed between two consecutive Volumes.""")
+		ch2 = int(input("Enter choice: "))
+		tmp = sp.call('clear', shell = True)
+
+		# 
+		# 
+		# 
+
+	else:
+		return
