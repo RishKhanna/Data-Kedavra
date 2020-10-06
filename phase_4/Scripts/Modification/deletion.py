007431568a5502fc453d1c50244f19d340069490
KyaBacchaHai <55993921+KyaBacchaHai@users.noreply.github.com>
2020-10-06 10:33:27 +0530

    Defined Modification  methods

---
+++ phase_4/Scripts/Modification/deletion.py
@@ -0,0 +1,11 @@
+def remove_a_member () :
+	pass
+
+def delete_an_article () :
+	pass
+
+def unsubscribe () :
+	pass
+
+def remove_a_sponsor () :
+	pass
---
+++ phase_4/Scripts/Modification/insertion.py
@@ -0,0 +1,20 @@
+def creating_an_issue () :
+	pass
+
+def adding_tags_to_article () :
+	pass
+
+def adding_categories_to_the_article () :
+	pass
+
+def adding_tags_to_the_article () :
+	pass
+
+def add_a_member () :
+	pass
+
+def add_a_subscriber () :
+	pass
+
+def add_a_sponsor () :
+	pass
