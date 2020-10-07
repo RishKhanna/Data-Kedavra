d4e265db0a5b27044028ca1071abfd447146e762
codelixir <pahulpreet.singh@research.iiit.ac.in>
2020-10-08 03:34:49 +0530

    insert lazy commit message

--- phase_4/Scripts/Retrival/selection.py
+++ phase_4/Scripts/Retrival/selection.py
@@ -1,54 +1,84 @@
-def  list_all_issues () :
+import pymysql
+import pymysql.cursors
+from tabulate import tabulate
+
+
+def db_con(username, password, query):
+    db = pymysql.connect("localhost", username, password, "data_kedavra")
+    with db.cursor() as cur:
+        cur.execute(query)
+        data = cur.fetchall()
+
+    return data
+
+
+def list_all_issues(username, password):
+    query = "SELECT * FROM MAGAZINE;"
+    table = db_con(username, password, query)
+    print(tabulate(table, headers=[
+          'Volume No', 'Issue No', 'Publication Date'], tablefmt='psql'))
+
+
+def list_all_articles(username, password):
+    query = "SELECT volume_no, issue_no, title, category FROM MAGAZINE;"
+    table = db_con(username, password, query)
+    print(tabulate(table, headers=[
+          'Vol No', 'Issue No', 'Title', 'Category'], tablefmt='psql'))
+
+
+def list_all_authors(username, password):
+    query = "SELECT id, f_name, m_name, l_name, languages, email, join_date FROM author INNER JOIN Members on id=member_id;"
+    table = db_con(username, password, query)
+    print(tabulate(table, headers=[
+          'Author ID', 'First Name', 'Middle Name', 'Last Name', 'Languages', 'E-mail', 'Date of Joining'], tablefmt='psql'))
+
+
+def articles_from_a_particular_magazine_issue(username, password):
     pass
 
 
-def  list_all_articles () :
+def list_of_all_articles_written_by_an_author(username, password):
+    id = input("Enter Author ID: ")
+    # Todo
+
+
+def list_all_editors(username, password):
+    query = "SELECT id, f_name, m_name, l_name, genres, email, join_date FROM editor INNER JOIN Members on id=member_id;"
+    table = db_con(username, password, query)
+    print(tabulate(table, headers=[
+          'Editor ID', 'First Name', 'Middle Name', 'Last Name', 'Genres', 'E-mail', 'Date of Joining'], tablefmt='psql'))
+
+
+def list_of_all_articles_edited_by_an_editor(username, password):
     pass
 
 
-def  list_all_authors () :
+def list_all_designers(username, password):
+    query = "SELECT id, f_name, m_name, l_name, softwaress, email, join_date FROM designer INNER JOIN Members on id=member_id;"
+    table = db_con(username, password, query)
+    print(tabulate(table, headers=[
+          'Designer ID', 'First Name', 'Middle Name', 'Last Name', 'Softwares', 'E-mail', 'Date of Joining'], tablefmt='psql'))
+
+
+def list_of_all_articles_designed_by_a_designer(username, password):
     pass
 
 
-def  articles_from_a_particular_magazine_issue () :
+def show_cartoons_by_designer(username, password):
     pass
 
 
-def  list_of_all_articles_written_by_an_author () :
+def show_cartoons_from_a_particular_issue(username, password):
     pass
 
 
-def  list_all_editors () :
+def show_cartoons_from_a_particular_volume(username, password):
     pass
 
 
-def  list_of_all_articles_edited_by_an_editor () :
+def subscribers_for_a_given_volume(username, password):
     pass
 
 
-def  list_all_designers () :
-    pass
-
-
-def  list_of_all_articles_designed_by_a_designer () :
-    pass
-
-
-def  show_cartoons_by_designer () :
-    pass
-
-
-def  show_cartoons_from_a_particular_issue () :
-    pass
-
-
-def  show_cartoons_from_a_particular_volume () :
-    pass
-
-
-def  subscribers_for_a_given_volume () :
-    pass
-
-
-def  subscribers_for_a_given_issue () :
+def subscribers_for_a_given_issue(username, password):
     pass
--- phase_4/main.py
+++ phase_4/main.py
@@ -7,87 +7,92 @@ from termcolor import colored
 
 
 def fn1():
-	query = "SHOW TABLES;"
-	cur.execute(query)
-	data = cur.fetchall()
-	for i in range(len(data)):
-		if i<10:
-			print(colored(str(i) +" : " + data[i][0], "yellow"))
-		else:
-			print(colored(str(i)+": " + data[i][0], "yellow"))
+    query = "SHOW TABLES;"
+    cur.execute(query)
+    data = cur.fetchall()
+    for i in range(len(data)):
+        if i < 10:
+            print(colored(str(i) + " : " + data[i][0], "yellow"))
+        else:
+            print(colored(str(i)+": " + data[i][0], "yellow"))
 
 # call the function depending on the option chosen
-def dispatch(ch):
-	if ch == 1:
-		fn1()
-	elif ch==2:
-		retrive.main()
-	elif ch==3:
-		modify.main(username, password)
 
-	else:
-		print(colored("Enter a valid option.", 'red'))
+
+def dispatch(ch):
+    if ch == 1:
+        fn1()
+    elif ch == 2:
+        retrive.main(username, password)
+    elif ch == 3:
+        modify.main(username, password)
+
+    else:
+        print(colored("Enter a valid option.", 'red'))
 
 
 while(1):
-	tmp = sp.call('clear', shell = True)
+    tmp = sp.call('clear', shell=True)
 
-	# get username and password through input
-	print(colored("Type exit to exit the application", 'red'))
-	username = input(colored("Username: ", 'cyan'))
-	if(username == "exit"):
-		break
-	password = input(colored("Password: ", 'cyan'))
+    # get username and password through input
+    print(colored("Type exit to exit the application", 'red'))
+    username = input(colored("Username: ", 'cyan'))
+    if(username == "exit"):
+        break
+    password = input(colored("Password: ", 'cyan'))
 
-	if(password == "exit"):
-		break
+    if(password == "exit"):
+        break
 
-	# establish a MySQL connection
-	try:
-		# connect to DB
-		db = pymysql.connect("localhost", username, password, "data_kedavra")
-		tmp = sp.call('clear', shell = True)
+    # establish a MySQL connection
+    try:
+        # connect to DB
+        db = pymysql.connect("localhost", username, password, "data_kedavra")
+        tmp = sp.call('clear', shell=True)
 
-		# show connection status to the user
-		# ASK KSHITIJAA ABOUT THIS
-		# if(db.opencur):
-		# 	print("Connected")
-		# else:
-		# 	print("lol F")
+        # show connection status to the user
+        # ASK KSHITIJAA ABOUT THIS
+        # if(db.opencur):
+        # 	print("Connected")
+        # else:
+        # 	print("lol F")
 
-		tmp = input(colored("Enter any key to CONTINUE IN THIS REALM>", 'green'))
+        tmp = input(
+            colored("Enter any key to CONTINUE IN THIS REALM>", 'green'))
 
-		with db.cursor() as cur:
+        with db.cursor() as cur:
 
-			# qr = "source dump.sql"
-			# cur.execute(qr)
-			# db.commit()
+            # qr = "source dump.sql"
+            # cur.execute(qr)
+            # db.commit()
 
+            while(1):
+                # print the options available
+                tmp = sp.call('clear', shell=True)
+                print(colored(
+                    "Which operations would you like to perform to the database?", 'blue', attrs=['bold']))
+                print(colored("1. Show Tables.", 'cyan', attrs=['bold']))
+                print(colored("2. Retrival", 'cyan', attrs=['bold']))
+                print(colored(
+                    "\tincludes: \n\t\t-> Selection.\n\t\t-> Projection.\n\t\t-> Aggregate.\n\t\t-> Search.\n\t\t-> Analysis.", 'yellow'))
+                print(colored("3. Modification", 'cyan', attrs=['bold']))
+                print(colored(
+                    "\tincludes:\n\t\t-> Insertion.\n\t\t-> Updation.\n\t\t-> Deletion.", 'yellow'))
+                print(colored("0. Logout", 'red', attrs=['bold']))
 
-			while(1):
-				# print the options available
-				tmp = sp.call('clear', shell = True)
-				print(colored("Which operations would you like to perform to the database?", 'blue',attrs=['bold']))
-				print(colored("1. Show Tables.", 'cyan', attrs=['bold']))
-				print(colored("2. Retrival", 'cyan', attrs=['bold']))
-				print(colored("\tincludes: \n\t\t-> Selection.\n\t\t-> Projection.\n\t\t-> Aggregate.\n\t\t-> Search.\n\t\t-> Analysis.", 'yellow'))
-				print(colored("3. Modification", 'cyan', attrs=['bold']))
-				print(colored("\tincludes:\n\t\t-> Insertion.\n\t\t-> Updation.\n\t\t-> Deletion.",'yellow'))
-				print(colored("0. Logout", 'red', attrs=['bold']))
+                # get user input for option
+                ch = int(input(colored("Enter choice: ", 'green')))
+                tmp = sp.call('clear', shell=True)
 
-				# get user input for option
-				ch = int(input(colored("Enter choice: ",'green')))
-				tmp = sp.call('clear', shell = True)
+                # to exit the option list
+                if ch == 0:
+                    break
+                else:
+                    dispatch(ch)
+                    tmp = input(colored("Press Enter to CONTINUE >", 'green'))
 
-				# to exit the option list
-				if ch == 0:
-					break
-				else:
-					dispatch(ch)
-					tmp = input(colored("Press Enter to CONTINUE >", 'green'))
-
-	# error handling
-	except Exception as e:
-		tmp = sp.call('clear', shell = True)
-		print(e)
-		tmp = input(colored("Press Enter to CONTINUE:",'green'))
+    # error handling
+    except Exception as e:
+        tmp = sp.call('clear', shell=True)
+        print(e)
+        tmp = input(colored("Press Enter to CONTINUE:", 'green'))
