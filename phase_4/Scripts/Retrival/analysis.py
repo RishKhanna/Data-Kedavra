0758e58529177c5a95cb3aea434c59fe9353c21a
codelixir <pahulpreet.singh@research.iiit.ac.in>
2020-10-08 21:51:03 +0530

    Analysis done and tested

--- phase_4/Scripts/Retrival/analysis.py
+++ phase_4/Scripts/Retrival/analysis.py
@@ -13,8 +13,18 @@ def db_con(username, password, query):
 
 
 def most_frequent_category_of_articles_written_by_each_author(username, password):
-    pass
+    id = input("Enter Author ID: ")
+    query = "SELECT CATEGORY FROM CONTRIBUTE INNER JOIN ARTICLE ON (CONTRIBUTE.VOLUME_NO, CONTRIBUTE.ISSUE_NO, CONTRIBUTE.PAGE_NO) = (ARTICLE.VOLUME_NO, ARTICLE.ISSUE_NO, ARTICLE.PAGE_NO) WHERE AUTHOR_ID=" + \
+        id + " GROUP BY CATEGORY ORDER BY COUNT(CATEGORY) DESC LIMIT 1;"
+    result = db_con(username, password, query)
+    print("Most frequent category: " + result[0][0])
 
 
 def list_of_subscribers_who_subscribed_between_two_consecutive_volumes(username, password):
-    pass
+    vol_id = int(input("First Volume: "))+1
+    print("Second Volume: " + str(vol_id))
+    query = "SELECT SUB_DATE, F_NAME, M_NAME, L_NAME, EMAIL FROM SUBSCRIBERS INNER JOIN SUBSCRIBES ON SUBSCRIBERS.EMAIL=SUBSCRIBES.SUBSCRIBER WHERE VOLUME =" + \
+        str(vol_id)+" ORDER BY SUB_DATE;"
+    table = db_con(username, password, query)
+    print(tabulate(table, headers=[
+          'Subscription Date', 'First Name', 'Middle Name', 'Last Name', 'E-Mail'], tablefmt='psql'))
